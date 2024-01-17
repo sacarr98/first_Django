a25c35359da2cf9c985f4ba194683278d0e36e4e
sacarr98 <>
2024-01-17 10:51:11 +0000

    add about page

--- hello_world/views.py
+++ hello_world/views.py
@@ -8,3 +8,7 @@ def index(request):
         return HttpResponse("You must have POSTed something")
     else:
         return HttpResponse(request.method)
+
+
+def about_me(request):
+    return HttpResponse("This would be the about page")
